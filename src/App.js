c32ef43020251d280442e12771aec22e7fc34b44
mohitzhcet <113200303+mohitzhcet@users.noreply.github.com>
2024-03-29 18:07:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './App.css';
+import {Box} from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from './components/Footer';
+
+
+const App = () => {
+  return (
+    <div>
+      <Box width="400px" sx={{width: {xl: '1488px'}}} m='auto'>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element= {<Home/>}/>
+          <Route path='/exercise/:id' element= {<ExerciseDetail/>}/>
+        </Routes>
+      </Box>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
